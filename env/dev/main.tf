0b70c8d86f5937184dfaab92521b6943c555cd97
root <root@ip-10-134-90-66.ap-northeast-2.compute.internal>
2025-06-23 02:06:45 +0000

    chore: update code & lockfile

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,21 +1,22 @@
-provider "aws" { region = "ap-northeast-2" }
-
-locals { azs = ["ap-northeast-2a","ap-northeast-2c"] }
+provider "aws" {
+  region = "ap-northeast-2"
+}
 
 module "vpc" {
-    source              = "../../module/vpc"
-      name_prefix         = var.project
-        cidr                = "10.30.0.0/16"
-          azs                 = local.azs
-            public_subnet_bits  = 8
-          }
+  source      = "../../module/vpc"
+  name        = var.project
+  cidr_block  = "10.30.0.0/16"
+}
 
-          module "bastion" {
-              source         = "../../module/ec2"
-                name           = "${var.project}-bastion"
-                  ami            = "ami-0c9c942bd7bf113a2"
-                    instance_type  = "t2.micro"
-                      subnet_id      = module.vpc.public_subnet_ids[0]
-                    }
+module "bastion" {
+  source        = "../../module/ec2"
+  name          = "${var.project}-bastion"
+  ami_id           = "ami-0c9c942bd7bf113a2"
+  instance_type = "t2.micro"
+  subnet_id     = module.vpc.public_subnet_ids[0] # ⚠️ 주의: 이 출력이 실제로 있어야 함
+}
 
-                    output "bastion_ip" { value = module.bastion.public_ip }
+output "bastion_ip" {
+  value = module.bastion.public_ip
+}
+
--- module/vpc/outputs.tf
+++ module/vpc/outputs.tf
@@ -1,4 +1,8 @@
 output "vpc_id"       { value = aws_vpc.this.id }
 output "subnet_id"    { value = aws_subnet.public.id }
 output "public_cidr"  { value = aws_subnet.public.cidr_block }
+output "public_subnet_ids" {
+    value = [aws_subnet.public.id]
+  }
+
 
